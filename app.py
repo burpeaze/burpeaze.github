fae4b37d90c0a4369cd66b8abc88bb42bdff7d8f
burpeaze <156306094+burpeaze@users.noreply.github.com>
2024-03-04 23:15:22 -0800

    updated files checkin

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template
-
+import datetime
+from flask_sqlalchemy import SQLAlchemy
+import sqlite3
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    current_year = datetime.datetime.now().year
+    return render_template('index.html', year_copy = current_year)
+
+@app.route('/get_started')
+def get_started():
+    return render_template('Get Started.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
